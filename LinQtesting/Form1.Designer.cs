0d9038ad72e10ef5a12dd5358ed2d71643a2d061
chulan.kumara <chulan.kumara@DESKTOP-4VO3P04>
2017-09-21 14:21:30 +0530

    Add project files.

---
+++ LinQtesting/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace LinQtesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_Result = new System.Windows.Forms.Button();
+            this.dataGridView_Result = new System.Windows.Forms.DataGridView();
+            this.testDataSet = new LinQtesting.testDataSet();
+            this.dboMemberDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nAMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aGEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gENDERDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cITYDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pOSTALCODEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dboMemberDetailsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Result
+            // 
+            this.button_Result.Location = new System.Drawing.Point(576, 355);
+            this.button_Result.Name = "button_Result";
+            this.button_Result.Size = new System.Drawing.Size(75, 23);
+            this.button_Result.TabIndex = 0;
+            this.button_Result.Text = "Query";
+            this.button_Result.UseVisualStyleBackColor = true;
+            this.button_Result.Click += new System.EventHandler(this.button_Result_Click);
+            // 
+            // dataGridView_Result
+            // 
+            this.dataGridView_Result.AutoGenerateColumns = false;
+            this.dataGridView_Result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Result.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nAMEDataGridViewTextBoxColumn,
+            this.aGEDataGridViewTextBoxColumn,
+            this.gENDERDataGridViewTextBoxColumn,
+            this.cITYDataGridViewTextBoxColumn,
+            this.pOSTALCODEDataGridViewTextBoxColumn});
+            this.dataGridView_Result.DataSource = this.dboMemberDetailsBindingSource;
+            this.dataGridView_Result.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView_Result.Name = "dataGridView_Result";
+            this.dataGridView_Result.Size = new System.Drawing.Size(638, 327);
+            this.dataGridView_Result.TabIndex = 1;
+            // 
+            // testDataSet
+            // 
+            this.testDataSet.DataSetName = "testDataSet";
+            this.testDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dboMemberDetailsBindingSource
+            // 
+            this.dboMemberDetailsBindingSource.DataMember = "dbo.MemberDetails";
+            this.dboMemberDetailsBindingSource.DataSource = this.testDataSet;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nAMEDataGridViewTextBoxColumn
+            // 
+            this.nAMEDataGridViewTextBoxColumn.DataPropertyName = "NAME";
+            this.nAMEDataGridViewTextBoxColumn.HeaderText = "NAME";
+            this.nAMEDataGridViewTextBoxColumn.Name = "nAMEDataGridViewTextBoxColumn";
+            // 
+            // aGEDataGridViewTextBoxColumn
+            // 
+            this.aGEDataGridViewTextBoxColumn.DataPropertyName = "AGE";
+            this.aGEDataGridViewTextBoxColumn.HeaderText = "AGE";
+            this.aGEDataGridViewTextBoxColumn.Name = "aGEDataGridViewTextBoxColumn";
+            // 
+            // gENDERDataGridViewTextBoxColumn
+            // 
+            this.gENDERDataGridViewTextBoxColumn.DataPropertyName = "GENDER";
+            this.gENDERDataGridViewTextBoxColumn.HeaderText = "GENDER";
+            this.gENDERDataGridViewTextBoxColumn.Name = "gENDERDataGridViewTextBoxColumn";
+            // 
+            // cITYDataGridViewTextBoxColumn
+            // 
+            this.cITYDataGridViewTextBoxColumn.DataPropertyName = "CITY";
+            this.cITYDataGridViewTextBoxColumn.HeaderText = "CITY";
+            this.cITYDataGridViewTextBoxColumn.Name = "cITYDataGridViewTextBoxColumn";
+            // 
+            // pOSTALCODEDataGridViewTextBoxColumn
+            // 
+            this.pOSTALCODEDataGridViewTextBoxColumn.DataPropertyName = "POSTALCODE";
+            this.pOSTALCODEDataGridViewTextBoxColumn.HeaderText = "POSTALCODE";
+            this.pOSTALCODEDataGridViewTextBoxColumn.Name = "pOSTALCODEDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 390);
+            this.Controls.Add(this.dataGridView_Result);
+            this.Controls.Add(this.button_Result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dboMemberDetailsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Result;
+        private System.Windows.Forms.DataGridView dataGridView_Result;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nAMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aGEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gENDERDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cITYDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pOSTALCODEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource dboMemberDetailsBindingSource;
+        private testDataSet testDataSet;
+    }
+}
+
---
+++ LinQtesting/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinQtesting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Result_Click(object sender, EventArgs e)
+        {
+            // Create a datacontext
+            LinQERDataContext con = new LinQERDataContext();
+
+            // Select all statement
+            //var LinQ = from d in con.dbo_MemberDetails select d;
+
+            // Select certain columns
+            //var LinQ = from d in con.dbo_MemberDetails select new {d.NAME, d.AGE, d.GENDER };
+
+            // Where Clause
+            //var LinQ = from d in con.dbo_MemberDetails where d.ID == 2 select d;
+
+            // Where Clause with 2 arguments AND operator
+            //var LinQ = from d in con.dbo_MemberDetails where d.ID == 2 && d.CITY == "NewJersey" select d;
+
+            // Where Clause with 2 arguments OR operator
+            //var LinQ = from d in con.dbo_MemberDetails where d.ID == 2 || d.CITY == "NewJersey" select d;
+
+            // Order By Clause
+            //var LinQ = from d in con.dbo_MemberDetails orderby d.NAME select d;
+
+            // Order By Clause Descending
+            //var LinQ = from d in con.dbo_MemberDetails orderby d.NAME descending select d;
+
+            // Order By Clause with 2 variables
+            //var LinQ = from d in con.dbo_MemberDetails orderby d.NAME, d.AGE select d;
+
+            // Top result query
+            //var LinQ = (from d in con.dbo_MemberDetails select d).Take(3);
+
+            // Top result query for the first result
+            //var LinQ = (from d in con.dbo_MemberDetails select d).Take(1);
+            // OR
+            //var LinQ = (from d in con.dbo_MemberDetails select d).First();
+
+            // Distinct query
+            //var LinQ = (from d in con.dbo_MemberDetails select new {d.POSTALCODE}).Distinct();
+            //var LinQ = (from d in con.dbo_MemberDetails select d).Distinct();
+
+            // Group by query
+            //var LinQ = from d in con.dbo_MemberDetails group d by d.ID into e select new {ID = e.Key};
+            // OR
+            //var LinQ = from p in con.dbo_MemberDetails
+            //           group p by p.GENDER into g
+            //           select new
+            //           {                                                
+            //               GENDER = g.Select(h => h.GENDER).FirstOrDefault(),
+            //               NAME = g.Select(h => h.NAME).FirstOrDefault()
+            //           };
+
+            // Union query
+            var LinQ = (from d in con.dbo_MemberDetails where d.NAME == "John" select d).Union(
+                from h in con.dbo_MemberDetails where h.NAME == "Mike" select h);
+
+            // Display data onto datagrid
+            dataGridView_Result.DataSource = LinQ;
+        }
+    }
+}
